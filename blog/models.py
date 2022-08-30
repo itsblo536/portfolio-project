00ce429b9433ab656904721344e2232869a568f2
“Brendan <itsblo2002@gmail.com>
2022-08-30 12:37:00 -0400

    Blog model up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+
+#Create a Blog models
+#title
+#publication date
+#body
+#image
+
+#Add the blog app to the settings
+
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
--- jobs/admin.py
+++ jobs/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Job
 
+# Allows us to change/add data without touching database
 admin.site.register(Job)
